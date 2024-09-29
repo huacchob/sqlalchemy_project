9316dd59fbf113848e701a4e6e3944721329f1e8
Carlos Huaccho <Carlos@gmail.com>
2024-09-28 21:25:48 -0400

    adding annotations to fill_docker_compose_secrets

--- fill_docker_compose_secrets.py
+++ fill_docker_compose_secrets.py
@@ -1,47 +1,50 @@
-from typing import Union, Optional, Dict, List
 import os
+import typing as t
+
 from yaml import safe_load, dump
+
 from python_scripts.utils import find_file_path, load_secrets_from_file, get_secret
 
-creds_env_file: Optional[str | None] = find_file_path("creds.env", __file__)
-docker_compose_file: Optional[str | None] = find_file_path(
+creds_env_file: t.Optional[str | None] = find_file_path("creds.env", __file__)
+docker_compose_file: t.Optional[str | None] = find_file_path(
     "docker-compose.yml", __file__
 )
 
 load_secrets_from_file("creds.env", __file__)
 
 with open(creds_env_file, "r", encoding="utf-8") as creds_file:
+    creds_file: t.TextIO
     secrets_values: str = creds_file.read()
-    secrets_values_list: List[str] = secrets_values.split("\n")
+    secrets_values_list: t.List[str] = secrets_values.split("\n")
 
 secrets = []
 
 for secret_pair in secrets_values_list:
     if secret_pair.startswith("#"):
         continue
-    split_secret: List[str] = secret_pair.split("=")
+    split_secret: t.List[str] = secret_pair.split("=")
     if os.environ.get(split_secret[0], None):
         secrets.append(split_secret[0])
 
 with open(docker_compose_file, "r", encoding="utf-8") as docker_compose_file_read:
-    docker_compose_config: Dict[str, Dict[str, Dict[str, Union[str, List[str]]]]] = (
-        safe_load(docker_compose_file_read)
-    )
+    docker_compose_config: t.Dict[
+        str, t.Dict[str, t.Dict[str, t.Union[str, t.List[str]]]]
+    ] = safe_load(docker_compose_file_read)
 
-db_secrets_path: List[str] = (
+db_secrets_path: t.List[str] = (
     docker_compose_config.get("services", {}).get("db", {}).get("environment", [])
 )
-pgadmin_secrets_path: List[str] = (
+pgadmin_secrets_path: t.List[str] = (
     docker_compose_config.get("services", {}).get("pgadmin", {}).get("environment", [])
 )
 
-total_secrets: List[str] = db_secrets_path + pgadmin_secrets_path
+total_secrets: t.List[str] = db_secrets_path + pgadmin_secrets_path
 
-postgres_secrets: List[str | None] = []
-pgadmin_secrets: List[str | None] = []
+postgres_secrets: t.List[str | None] = []
+pgadmin_secrets: t.List[str | None] = []
 
 for secret in total_secrets:
-    docker_secret_value: List[str] = secret.split("=")
+    docker_secret_value: t.List[str] = secret.split("=")
     secret_name: str = docker_secret_value[0]
     if secret_name not in secrets:
         raise ValueError(f"Secret {secret_name} not found in creds.env file")
@@ -55,4 +58,5 @@ docker_compose_config.get("services").get("db")["environment"] = postgres_secret
 docker_compose_config.get("services").get("pgadmin")["environment"] = pgadmin_secrets
 
 with open(docker_compose_file, "w", encoding="utf-8") as docker_compose_file_write:
+    docker_compose_file_write: t.TextIO
     dump(docker_compose_config, docker_compose_file_write)
